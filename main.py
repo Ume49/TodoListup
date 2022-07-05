dfd4347b61ecc8149f97a76487ab1cad1ff71791
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-05 09:58:25 +0900

    簡単にウィジェット追加

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
-import tkinter
-import tkinter.ttk
+from tkinter import *
+from tkinter import ttk
 
-from pip import main
+root_window = Tk()
 
-main_window = tkinter.Tk()
+root_window.title("TODO ListUp forUnity")
 
-main_window.title("TODO ListUp forUnity")
+frame = ttk.Frame(root_window, padding=16).pack()
 
-main_window.mainloop()
+ttk.Label(
+    frame,
+    text    = "根元のディレクトリ"
+).pack(side = LEFT)
+
+ttk.Entry(
+    frame
+
+).pack(side = LEFT)
+
+ttk.Button(
+    frame,
+    text    = "...",
+    command = lambda: Tk()
+).pack(side = LEFT)
+
+root_window.mainloop()
