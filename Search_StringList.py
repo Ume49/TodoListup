a8d6a27050ac3fef0f4ac2687d76334bfc1196cb
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-07 14:31:36 +0900

    ファイル内検索用関数を実装

--- Search_StringList.py
+++ Search_StringList.py
@@ -1,10 +1,9 @@
-
+import os
+import Result_Component
 
 # "// TODO"という文字の後に続く行を探してリストアップする
 # ファイルパスは絶対パス渡してね
-def Search_behindTODO(file_path = "", find_tag = "", output_list = []):
-    encode_option = "utf-8_sig"
-
+def Search_behindTODO(file_path = "", find_tag = "// TODO ", encode_option = "utf-8_sig", output_list = []):
     file_data = open(file_path, encoding=encode_option)
 
     # 行番号カウンタ
@@ -20,9 +19,14 @@ def Search_behindTODO(file_path = "", find_tag = "", output_list = []):
         # 見つからん場合は次の行へ
         if todo_index == -1: continue
 
-        # TODO以降の文章をリストに追加
-        # TODO リストの形式が後で変わるので、その時に合わせてここも変える
-        output_list.append(line[todo_index+len(find_tag) : ])
+        # TODO以降の文章を取得
+        sentence = line[todo_index + len(find_tag) : ]
+        
+        # パスからファイル名を抽出
+        file_name = os.path.basename(file_path)
+
+        # 返却用リストに項目を追加
+        output_list.append(Result_Component.Result_Component(sentence=sentence, file_name=file_name, line=line_num))
 
     file_data.close
 
@@ -30,12 +34,3 @@ def Search_behindTODO(file_path = "", find_tag = "", output_list = []):
 
 if __name__ == "__main__":
     dir = "D:\E_repos\Tumi_Dungeon\詰めダンジョン\Assets\Script\Charactor\Charactor_Paramater.cs"
-
-    file_data = open(dir, "r", encoding="utf-8_sig")
-
-    for line in file_data:
-        index = line.find("using")
-        if index != -1:
-            print(line[index+5 : ])
-
-    file_data.close()
