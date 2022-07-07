5dda08f1724f18dc854aa582eab81fd1aeb7fd9c
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-07 14:17:39 +0900

    使用例追加

--- Result_Component.py
+++ Result_Component.py
@@ -1,7 +1,7 @@
 # 表示リストに使用するクラスというか構造体
-
+# 使用例： 後で中身を記述する (sentence) TestCode.py (file_name) 32行目 (line)
 class Result_Component:
     def __init__(self, sentence="", file_name="", line=0) :
-        self.sentence   = sentence
-        self.file_name  = file_name
-        self.line       = line
+        self.sentence   = sentence      # コメント
+        self.file_name  = file_name     # ファイル名
+        self.line       = line          # そのファイルの何行目にあるコメントなのか
