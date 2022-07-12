24e57c0e1af1afb0c08022adf2d6b5b217df6a71
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-12 15:18:45 +0900

    ...ボタンを押したときにフォルダを選べるようにした

--- Widget.py
+++ Widget.py
@@ -1,80 +1,89 @@
 # ウィジェットの配置をまとめたパッケージ
+# ここのクラスを使うときは、インスタンスを生成 → インスタンス.root.pack(オプション) → インスタンス.Member_Pack()の順番で呼んでね
+
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 
 # ディレクトリを聞くためのフォーム
 class Ask_Directory_Form:
-    def __init__(self, root_tkobj):
+    def __init__(this, root):
         # 根元のフレーム
-        self.root = ttk.Frame(
-            root_tkobj
-        )
+        this.root = ttk.Frame(root)
 
         # ラベル
-        self.label = ttk.Label(
-            self.root,
+        this.label = ttk.Label(
+            this.root,
             text="検索するフォルダ"
         )
 
         # 入力フォームとボタンを横に並べたいのでそれをまとめるフレーム
-        self.entry_frame = ttk.Frame(
-            self.root
+        this.entry_frame = ttk.Frame(
+            this.root
         )
 
+        # 入力内容
+        this.entry_value = StringVar()
+
         # 入力フォーム
-        self.entry = ttk.Entry(
-            self.entry_frame
+        this.entry = ttk.Entry(
+            this.entry_frame,
+            textvariable = this.entry_value,
+            width=60
         )
         
         # 入力フォームの横のボタン
-        self.button = ttk.Button(
-            self.entry_frame,
+        this.button = ttk.Button(
+            this.entry_frame,
             text    = "...",
-            width   = 5
+            width   = 5,
+            command = this.Button_Directory
         )
 
-    def Pack(self):
+    def Member_Pack(this):
         # メンバを配置
-        self.root       .pack()
-        self.label      .pack(side=TOP, anchor="nw")
-        self.entry_frame.pack(side=TOP)
-        self.entry      .pack(side=LEFT)
-        self.button     .pack(side=LEFT)
+        this.label      .pack(side=TOP, anchor="nw")
+        this.entry_frame.pack(side=TOP)
+        this.entry      .pack(side=LEFT)
+        this.button     .pack(side=LEFT)
+
+    # ボタンを押したときのディレクトリ取得処理
+    def Button_Directory(this):
+        # ディレクトリを取得
+        result_directory = filedialog.askdirectory()
+
+        # 入力ボックスの中に内容を記述
+        this.entry_value.set(result_directory)
 
 # 検索キーワードを聞くためのフォーム
 class Ask_FindTag_Form:
-    def __init__(self, root):
+    def __init__(this, root):
         # このクラスで扱うウィジェットの受け皿
-        self.frame = ttk.Frame(
-            root
-        )
+        this.root = ttk.Frame(root)
         
         # ラベル
-        self.label = ttk.Label(
-            self.frame,
+        this.label = ttk.Label(
+            this.root,
             text    = "検索ワード"
         )
 
         # 入力フォーム
-        self.entry = ttk.Entry(
-            self.frame
+        this.entry = ttk.Entry(
+            this.root
         )
-    def Pack(self):
-        self.frame.pack()
-        self.label.pack(side=TOP, anchor="nw")
-        self.entry.pack(side=LEFT)
+    def Member_Pack(this):
+        this.label.pack(anchor="nw")
+        this.entry.pack()
 
 # エンコード形式の入力フォーム
 class Ask_Encode:
     def __init__(this, root):
         # 受け皿
-        this.frame = ttk.Frame(
-            root
-        )
+        this.root = ttk.Frame(root)
 
         # ラベル
         this.label = ttk.Label(
-            this.frame,
+            this.root,
             text = "エンコード"
         )
 
@@ -87,15 +96,14 @@ class Ask_Encode:
 
         # 入力リスト
         this.list = ttk.Combobox(
-            this.frame,
+            this.root,
             values = encode_list
         )
 
         # 先頭のやつを事前に登録しておく
         this.list.set(encode_list[0])
 
-    def Pack(this):
-        this.frame.pack()
+    def Member_Pack(this):
         this.label.pack(anchor="w")
         this.list .pack(anchor="w")
 
@@ -103,12 +111,10 @@ class Ask_Encode:
 class Ask_file_exstension:
     def __init__(this, root):
         # 受け皿
-        this.frame = ttk.Frame(
-            root
-        )
+        this.root = ttk.Frame(root)
 
         this.label = ttk.Label(
-            this.frame,
+            this.root,
             text = "拡張子"
         )
 
@@ -122,23 +128,35 @@ class Ask_file_exstension:
         ]
 
         this.list = ttk.Combobox(
-            this.frame,
+            this.root,
             values=ex_list
         )
 
         # 候補の先頭を事前に表示しておく
         this.list.set(ex_list[0])
 
-    def Pack(this):
-        this.frame.pack()
+    def Member_Pack(this):
         this.label.pack(anchor="w")
         this.list .pack(anchor="w")
+
+class Execute_Button:
+    def __init__(this, root):
+        this.root = Frame(root, pady = 10)
+
+        this.button = ttk.Button(
+            this.root,
+            text = "実行！"
+        )
+    
+    def Member_Pack(this):
+        this.button.pack(anchor="e")
 
 if __name__ == "__main__":
     t = Tk()
 
-    widgets = Ask_file_exstension(t)
+    widgets = Ask_Directory_Form(t)
     
-    widgets.Pack()
+    widgets.root.pack()
+    widgets.Member_Pack()
 
     t.mainloop()
--- main.py
+++ main.py
@@ -6,20 +6,36 @@ root_window = Tk()
 
 root_window.title("TODO ListUp forUnity")
 
-root_window.geometry("400x300")
+root_window.geometry("480x240")
 
 
 # ウィジェット配置
 
-widgets = []
+root_frame = Frame(root_window, pady= 10)
 
-widgets.append(Widget.Ask_Directory_Form (root_window))
-widgets.append(Widget.Ask_FindTag_Form   (root_window))
-widgets.append(Widget.Ask_file_exstension(root_window))
-widgets.append(Widget.Ask_Encode         (root_window))
+directory   = Widget.Ask_Directory_Form (root_frame)
+findtag     = Widget.Ask_FindTag_Form   (root_frame)
+file_ex     = Widget.Ask_file_exstension(root_frame)
+encode      = Widget.Ask_Encode         (root_frame)
+button      = Widget.Execute_Button     (root_frame)
 
-for w in widgets:
-    w.Pack()
+# 配置
+root_frame.pack()
+
+directory.root.pack(anchor="w")
+directory.Member_Pack()
+
+findtag.root.pack(anchor="w")
+findtag.Member_Pack()
+
+file_ex.root.pack(anchor="w")
+file_ex.Member_Pack()
+
+encode.root.pack(anchor="w")
+encode.Member_Pack()
+
+button.root.pack(expand=1)
+button.Member_Pack()
 
 # 使用予定
 # list = Search_TODO.SearchString_behindTODO("directory")
