34c67a88d6794ae7bdf462f23f1bf44942e769bd
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-13 14:21:57 +0900

    Display完成
    新たなウインドウを作成する方法をTk()からToplevel()に変更
    
    あとはListBoxに内容を追加する処理を実装

--- Display.py
+++ Display.py
@@ -1,15 +1,30 @@
-from tkinter import Listbox, Tk
-import Result_Component
+from tkinter import *
+from tkinter import ttk
 
 # 得られた結果を表示するクラス
 class Display:
-    def __init__(this, component=[Result_Component.Result_Component()]):
-        this.list = component
-        
+    def __init__(this, root_window):
         # ウインドウを作成
-        this.window = Tk()
+        this.window = Toplevel(root_window)
+
+        this.window.geometry("640x480")
 
         # リストを表示する場所を作成
-        this.listbox = Listbox(this.window, listvariable = this.list)
+        this.listbox = Listbox(this.window, selectbackground="#00bfff", width=103, height=28)
 
         this.listbox.pack()
+
+    # 表示する内容を追加
+    def Add_list(this, list=[]):
+        for w in list:
+            this.listbox.insert(END, w)
+
+# デバッグ用コード
+if __name__ == "__main__":
+    _root = Tk()
+
+    _disp = Display(_root)
+
+    _disp.Add_list(["あいうえお", "かきくけこ"])
+
+    _root.mainloop()
