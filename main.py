b8d035de2432cbbe904978bf59871c6a298c0bac
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-13 14:24:22 +0900

    実行ボタン実装
    入力された情報をもとに検索を実行するボタンを実装

--- main.py
+++ main.py
@@ -1,44 +1,48 @@
 from tkinter import *
+from tkinter import * 
 from tkinter import ttk
+from Display import Display
 import Widget
+from Execute import Execute
 
-root_window = Tk()
+_root_window = Tk()
 
-root_window.title("TODO ListUp forUnity")
+_root_window.title("TODO ListUp forUnity")
 
-root_window.geometry("480x240")
-
+_root_window.geometry("480x240")
 
 # ウィジェット配置
 
-root_frame = Frame(root_window, pady= 10)
+_root_frame = Frame(_root_window, pady= 10)
 
-directory   = Widget.Ask_Directory_Form (root_frame)
-findtag     = Widget.Ask_FindTag_Form   (root_frame)
-file_ex     = Widget.Ask_file_exstension(root_frame)
-encode      = Widget.Ask_Encode         (root_frame)
-button      = Widget.Execute_Button     (root_frame)
+_directory   = Widget.Ask_Directory_Form (_root_frame)
+_findtag     = Widget.Ask_FindTag_Form   (_root_frame)
+_file_ex     = Widget.Ask_file_exstension(_root_frame)
+_encode      = Widget.Ask_Encode         (_root_frame)
+_display     = Display(_root_window)
+
+_button = ttk.Button(
+    _root_frame,
+    text="検索！",
+    command = lambda:
+        Execute(_directory.get(), _findtag.get(), _file_ex.get(), _encode.get(), _display)
+)
 
 # 配置
-root_frame.pack()
+_root_frame.pack()
 
-directory.root.pack(anchor="w")
-directory.Member_Pack()
+_directory.root.pack(anchor="w")
+_directory.Member_Pack()
 
-findtag.root.pack(anchor="w")
-findtag.Member_Pack()
+_findtag.root.pack(anchor="w")
+_findtag.Member_Pack()
 
-file_ex.root.pack(anchor="w")
-file_ex.Member_Pack()
+_file_ex.root.pack(anchor="w")
+_file_ex.Member_Pack()
 
-encode.root.pack(anchor="w")
-encode.Member_Pack()
+_encode.root.pack(anchor="w")
+_encode.Member_Pack()
 
-button.root.pack(expand=1)
-button.Member_Pack()
+_button.pack()
 
-# 使用予定
-# list = Search_TODO.SearchString_behindTODO("directory")
-# listには表示すべき文字列リストが返ってくる
-
-root_window.mainloop()
+_root_window.mainloop()
