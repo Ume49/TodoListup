12032e01942a94f2b9123e7364e66a580141a438
K2Cr2O7_laptop <tomoki@laptop.com>
2022-07-13 14:25:29 +0900

    .get()を追加
    入力された値を返す関数get()を追加

--- Widget.py
+++ Widget.py
@@ -1,10 +1,11 @@
 # ウィジェットの配置をまとめたパッケージ
 # ここのクラスを使うときは、インスタンスを生成 → インスタンス.root.pack(オプション) → インスタンス.Member_Pack()の順番で呼んでね
 
-from sys import displayhook
 from tkinter import *
 from tkinter import ttk
 from tkinter import filedialog
+
+from Display import Display
 
 # ディレクトリを聞くためのフォーム
 class Ask_Directory_Form:
@@ -55,6 +56,10 @@ class Ask_Directory_Form:
 
         # 入力ボックスの中に内容を記述
         this.entry_value.set(result_directory)
+    
+    # 入力フォームに書かれた内容を返す
+    def get(this):
+        return this.entry.get()
 
 # 検索キーワードを聞くためのフォーム
 class Ask_FindTag_Form:
@@ -75,6 +80,9 @@ class Ask_FindTag_Form:
     def Member_Pack(this):
         this.label.pack(anchor="nw")
         this.entry.pack()
+    
+    def get(this):
+        return this.entry.get()
 
 # エンコード形式の入力フォーム
 class Ask_Encode:
@@ -108,6 +116,9 @@ class Ask_Encode:
         this.label.pack(anchor="w")
         this.list .pack(anchor="w")
 
+    def get(this):
+        return this.list.get()
+
 # 拡張子の入力フォーム
 class Ask_file_exstension:
     def __init__(this, root):
@@ -140,31 +151,36 @@ class Ask_file_exstension:
         this.label.pack(anchor="w")
         this.list .pack(anchor="w")
 
+    def get(this):
+        return this.list.get()
+
 # 実行ボタン
 class Execute_Button:
-    def __init__(this, root):
+    def __init__(this, root, func):
         this.root = Frame(root, pady = 10)
 
         this.button = ttk.Button(
             this.root,
-            text = "実行！",
-            command = this.Execute
+            text = "検索して追加",
+            command=func
         )
+
+        # Null初期化の代わりに0を入れておく
+        this.extra_window = 0
     
     def Member_Pack(this):
         this.button.pack(anchor="e")
-    
-    def Execute(this):
-        import Display
-        list = ["a", "b", "100", "200"]
-        this.extra_window = Display.Display(list)
 
 if __name__ == "__main__":
     t = Tk()
 
-    widgets = Ask_Directory_Form(t)
+    widgets = Execute_Button(t)
     
     widgets.root.pack()
     widgets.Member_Pack()
+
+    widgets.Set_Result_List(["コード書け", "ゲーム作れ", "ツール完成させろ"])
+
+    widgets.Set_Root_Window(t)
 
     t.mainloop()
